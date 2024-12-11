966e2dafb7472dda907f44b8540ebc8a1a19159e
Leonardo Palavecino <64588981+leo43161@users.noreply.github.com>
2024-12-11 15:02:22 -0300

    Se cambio el link para que coincida y no de error en netlify

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     env: {
         URL_IMG: "https://d1v7vlxk2kna8x.cloudfront.net/",
-        URL_SERVER: 'http://10.20.20.5:3001/api/'
+        URL_SERVER: 'https://catgy.netlify.app/api/'
     }
 };
 
