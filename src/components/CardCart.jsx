d1a7cb136cbc3c9dcec3b53f287a1fb6514231ba
Leonardo Palavecino <64588981+leo43161@users.noreply.github.com>
2024-12-16 15:55:39 -0300

    Se agrego detalles a la seccion cart

--- src/components/CardCart.jsx
+++ src/components/CardCart.jsx
@@ -1,10 +1,11 @@
+import { Minus } from 'lucide-react'
 import React from 'react'
 
 export default function CardCart() {
     return (
         <>
-            <div className="p-4 border">
-                <div className="flex items-center gap-4">
+            <div className="p-4">
+                <div className="flex items-center gap-3">
                     <div className="rounded h-28 w-28 border overflow-hidden flex items-center justify-center">
                         <img
                             src={`${process.env.URL_IMG}products/1733491018548-464309961_850446757074196_539282240995737463_n.jpg`}
@@ -12,14 +13,21 @@ export default function CardCart() {
                         />
                     </div>
                     <div className='flex-1'>
-                        <h3 className="text-lg font-bold">Sorrentinos Argentinos</h3>
+                        <div className='flex flex-col gap-1'>
+                            <h3 className="font-bold">Sorrentinos Argentinos</h3>
+                            {/* <p className="text-sm text-gray-600">Pastas</p> */}
+                            <h3 className="text-lg font-bold"><span className='font-bold text-primary'>$</span>500</h3>
+                            <textarea className='w-full text-sm py-1 px-2 resize-y' rows={3} defaultValue={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique velit aperiam sit facere ipsam ea cumque in'}></textarea>
+                        </div>
                     </div>
-                    <div className='flex-1'>
-
+                    <div className='flex justify-center items-center'>
+                        <div className='border rounded-full p-1 bg-red-500 shadow-sm'>
+                            <Minus className="text-black" size={20} />
+                        </div>
                     </div>
                 </div>
             </div>
-
+            <hr className='mx-4' />
         </>
     )
 }
--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,22 +1,46 @@
 import CardCart from '@/components/CardCart'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Cart() {
+  const [seccion, setSeccion] = useState(true)
   return (
     <>
-      <div className="min-h-svh">
-        <CardCart></CardCart>
-      </div>
-      <div className="sticky bottom-0 left-0 w-full bg-gray-100 py-5">
-        <div className='flex items-center h-full px-4 justify-between'>
-          <div className='flex flex-col justify-center items-center'>
-            <p className="text-2xl font-bold"><span className='font-bold text-primary'>$</span>12000</p>
+      <div className="w-full flex justify-center py-1">
+        <div className="flex justify-center w-3/5 bg-gray-100 rounded">
+          <div onClick={() => setSeccion(true)} className={`flex-1 text-center rounded-lg py-1 ${seccion && 'text-white bg-primary'}`}>
+            <h1 className="font-bold">Pedido</h1>
           </div>
-          <div>
-            <button className="rounded ml-2 shadow text-white bg-primary py-2 px-4 font-bold">Agregar a la orden</button>
+          <div onClick={() => setSeccion(false)} className={`flex-1 text-center rounded-lg py-1 ${!seccion && 'text-white bg-primary'}`}>
+            <h1 className="font-bold">Ordenes</h1>
           </div>
         </div>
       </div>
+      {/* PEDIDO */}
+      {seccion && <>
+        <div className="min-h-svh">
+          <CardCart></CardCart>
+          <CardCart></CardCart>
+          <CardCart></CardCart>
+        </div>
+        <div className="sticky bottom-0 left-0 w-full bg-gray-100 py-5">
+          <div className='flex items-center h-full px-4 justify-between'>
+            <div className='flex flex-col justify-center items-center'>
+              <p className="text-2xl font-bold"><span className='font-bold text-primary'>$</span>12000</p>
+            </div>
+            <div>
+              <button className="rounded ml-2 shadow text-white bg-primary py-2 px-4 font-bold w-44">Ordenar</button>
+            </div>
+          </div>
+        </div>
+      </>}
+      {/* FIN PEDIDO */}
+      {/* ORDENES */}
+      {!seccion && <>
+        <div className="min-h-svh">
+          Orden
+        </div>
+      </>}
+      {/* FIN ORDENES */}
     </>
   )
 }
